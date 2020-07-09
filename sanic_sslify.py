535ea1f2ed139e7d406a1733b9ddc7f083999816
tomaszdrozdz <tomasz.drozdz.1@protonmail.com>
2020-07-09 20:14:36 +0200

    Improving.

--- sanic_sslify.py
+++ sanic_sslify.py
@@ -8,10 +8,12 @@ class SSLify():
 
     _instance = None
 
+
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
             cls._instance = super().__new__(cls)
         return cls._instance
+
 
     def __init__(self, app, hsts_age=None, hsts_include_subdomains=None, permanent_redirection=None, paths_to_skip=None):
         self.app = app
@@ -44,10 +46,10 @@ class SSLify():
             self.app.config['SSLIFY_PATHS_TO_SKIP'] = []
         self.paths_to_skip = self.app.config['SSLIFY_PATHS_TO_SKIP']
 
-
         """Configures the configured Sanic app to enforce SSL."""
         app.register_middleware(self.redirect_to_ssl, attach_to='request')
         app.register_middleware(self.set_hsts_header, attach_to='response')
+
 
     @property
     def hsts_header(self):
@@ -56,37 +58,36 @@ class SSLify():
 
         if self.hsts_include_subdomains:
             hsts_policy += '; includeSubDomains'
-
         return hsts_policy
 
-    def is_skip(self,request):
-        """Checks the skip list."""
-        # Should we skip?
+
+    def path_is_to_skip(self, request):
+        """Checks if path is in the skip paths list."""
         for path_to_skip in self.paths_to_skip:
             if request.path.startswith('/{0}'.format(path_to_skip)):
                 return True
         return False
 
-    def redirect_to_ssl(self,request):
+
+    def redirect_to_ssl(self, request):
         """Redirect incoming requests to HTTPS."""
         # Should we redirect?
         criteria = [
             request.scheme == 'https',
             self.app.debug,
-            request.headers.get('X-Forwarded-Proto', 'http') == 'https'
-        ]
+            request.headers.get('X-Forwarded-Proto', 'http') == 'https']
 
-        if not any(criteria) and not self.is_skip(request):
+        if not any(criteria) and not self.path_is_to_skip(request):
             if request.url.startswith('http://'):
                 url = request.url.replace('http://', 'https://', 1)
                 status = 302
                 if self.permanent_redirection:
                     status = 301
-                r = redirect(url, status=status)
-                return r
+                return redirect(url, status=status)
 
-    def set_hsts_header(self, request,response):
+
+    def set_hsts_header(self, request, response):
         """Adds HSTS header to each response."""
         # Should we add STS header?
-        if not self.is_skip(request):
+        if not self.path_is_to_skip(request):
             response.headers.setdefault('Strict-Transport-Security', self.hsts_header)
